a600fbddd7179fc9ab33203a2651db11d5a33b1b
solisllanosdafne-netizen <solisllanosdafne@gmail.com>
2026-03-17 19:49:27 -0300

    Enhance clase1703.py with variable and size info
    Added explanations for alphanumeric variables and data size conversions.

--- clase1703.py
+++ clase1703.py
@@ -2,3 +2,8 @@ las variables son 3, numericas
         entera
         decimas
         alfanumerica
+las alfa numericas son por ejemplo
+cada 8 BIT es 1 BYTE
+1024 BYTE es 1 KB
+1024 KB es 1 MB
+1024 MB es 1 GB                                                                                                           
