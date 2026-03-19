d2026c0a654a3cea0bbd3301bfb02326886aaef4
solisllanosdafne-netizen <solisllanosdafne@gmail.com>
2026-03-18 21:31:42 -0300

    Update input prompts and print statements
    Refactor input handling and output formatting.

--- clase1803.py
+++ clase1803.py
@@ -8,3 +8,18 @@ nom=input("ingrese talla de polera")
 2 formas para # salida 
 print("hola ,nom,"tienes" ,ed, "años, tu correo es :")
 print(f"hola {nom} tienes {ed} años,tu correo es:v{correo})
+#Declaración de variables
+nom = ""
+ed = 0
+correo = ""
+
+
+#Entrada
+nom = ""; nom = input("Ingrese nombre: ")
+ed = input("Su edad: ")
+correo = input("Correo electrónico: ")
+
+#Salida
+print(nom, ed, correo, sep=";")
+
+print(f"Hola {nom} tienes {ed} años, tu correo es : {correo}")
